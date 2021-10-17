0a0ef99d39eda10672ef520a60e7b1fb8f1fa26b
SonaliMane <sonalimane217@gmail.com>
2021-10-17 19:48:42 +0530

    test2 and test 3

--- incubyte/src/calculator/StringCalculatorTest.java
+++ incubyte/src/calculator/StringCalculatorTest.java
@@ -17,6 +17,15 @@ public class StringCalculatorTest {
 		assertEquals(0, Calculator.add(""));
 	}
 
+	@Test
+	public void testOneNumber() {
+		assertEquals(1, Calculator.add("1"));
+	}
+
+	@Test
+	public void testTwoNumbers(){
+		assertEquals(3, Calculator.add("1,2"));
+	}
 	
 	
 	
